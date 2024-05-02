a9de116a99c8b529cac847e71915750afb7b657a
Khawla-1006 <khawla.ben.in@gmail.com>
2024-05-03 01:08:16 +0200

    filtering displayed elements lesson

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,41 @@ const [notes, setNotes] = useState(props.notes)
 const [newNote, setNewNote] = useState(
   'a new note...'
 )
+const [showAll, setShowAll] = useState(true)
 
 
 const addNotes = (event) =>{
-event.preventDefault()
-// console.log('button clicked', event.target);
-// console.log(event);
-const noteObject = {
-  content: newNote,
-  important: Math.random() < 0.5,
-  id: notes.length + 1,
-}
-
-setNotes(notes.concat(noteObject));
-setNewNote('')
+    event.preventDefault()
+    // console.log('button clicked', event.target);
+    // console.log(event);
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+    setNotes(notes.concat(noteObject));
+    setNewNote('add another note!')
 }
 
 const handleNoteChange = (event) =>{
   // console.log(event.target.value);
- setNewNote(event.target.value)
+    setNewNote(event.target.value)
 }
+
+const notesToShow = showAll 
+        ? notes
+        : notes.filter(note => note.important === true) 
 
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
